fb6bca13d3f36cb9a0fea0627f7d6151f8c5040a
fmc <fmc@skynet3.local>
2018-02-19 11:13:12 +1000

    newtyrone

--- New.py
+++ New.py
@@ -2,5 +2,5 @@ name = ''
 while name != 'your name':
     print('Please type your name.')
     name = input()
-    print('Stank you very much sheeeet')
-    
+
+print('Stank you very much tyrone sheeeet')
