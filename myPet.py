7a8507e65c2e2e9d58f162eac005f13f42656fd8
john <john@somemail.com>
2018-06-19 13:45:47 +1000

    new

--- myPet.py
+++ myPet.py
@@ -1,8 +1,7 @@
-myPets = ['Zophie', 'Pooka', 'Fat tail']
+myPets = ['Zophie', 'Pooka', 'Fat-tail']
 print('Enter a pet name: ')
-if name not in myPets:
+name = input()
+if name not in myPets:
     print('I dont have a pet named ' + name)
-    else
+else:
     print(name + ' is my pet. ')
-    
-
