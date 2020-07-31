13a066b19c3baecb618a4c9393cfeac4a7bd32e2
pozz <zaphook@outlook.com>
2020-07-31 13:52:49 +1000

    new

--- whilespam1.py
+++ whilespam1.py
@@ -1,4 +1,5 @@
 spam = 0
-while spam < 5:
-    print('Hello, world.')
+while spam  < 5:
+    print('spam')
     spam = spam + 1
+
--- yourName2.py
+++ yourName2.py
@@ -1,6 +1,5 @@
-while True:
+name = ''
+while name != 'your name':
     print('Please type your name.')
     name = input()
-    if name == 'your name':
-         break
-print('Thank you!')
+    
